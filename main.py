275ea02880849d1c4ae351d64268b19b6912581d
leticia <braz-le@outlook.com>
2023-06-08 19:35:35 -0300

    Novas

--- main.py
+++ main.py
@@ -15,24 +15,24 @@ produtos_estoque = {
 
 
 produtos_vendidos = {
-    1: {"Item": "Trator Verde", "Preço unitário": "R$ 300.000,00", "Quantidade em estoque": 3},
-    2: {"Item": "Trator Amarelo", "Preço unitário": "R$ 300.000,00", "Quantidade em estoque": 8},
-    3: {"Item": "Trator Azul", "Preço unitário": "R$ 300.000,00", "Quantidade em estoque": 12},
-    4: {"Item": "Trator Vermelho", "Preço unitário": "R$ 280.000,00", "Quantidade em estoque": 2},
-    5: {"Item": "Trator de Colheita", "Preço unitário": "R$170.000,00", "Quantidade em estoque": 20},
-    6: {"Item": "Trator de Fertilizacao", "Preço unitário": "R$170.000,00", "Quantidade em estoque": 20},
-    7: {"Item": "Trator de Colheita", "Preço unitário": "R$570.000,00", "Quantidade em estoque": 28},
-    8: {"Item": "Trator Esportivo", "Preço unitário": "R$470.000,00", "Quantidade em estoque": 42},
+    1: {"Item": "Trator Verde", "Preço unitário": "R$ 300.000,00", "Quantidade vendida": 3},
+    2: {"Item": "Trator Amarelo", "Preço unitário": "R$ 300.000,00", "Quantidade vendida": 8},
+    3: {"Item": "Trator Azul", "Preço unitário": "R$ 300.000,00", "Quantidade vendida": 12},
+    4: {"Item": "Trator Vermelho", "Preço unitário": "R$ 280.000,00", "Quantidade vendida": 2},
+    5: {"Item": "Trator de Colheita", "Preço unitário": "R$170.000,00", "Quantidade vendida": 20},
+    6: {"Item": "Trator de Fertilizacao", "Preço unitário": "R$170.000,00", "Quantidade vendida": 20},
+    7: {"Item": "Trator de Colheita", "Preço unitário": "R$570.000,00", "Quantidade vendida": 28},
+    8: {"Item": "Trator Esportivo", "Preço unitário": "R$470.000,00", "Quantidade vendida": 42},
 }
 
 @app.get("/")
 def home():
-    return {"Produtos no estoque": len(produtos_estoque)}
+    return {"Tipos de tratotes": len(produtos_estoque)}
 
 @app.get("/estoque/{id_estoque}")
 def pegar_estoque(id_estoque: int):
     if id_estoque in produtos_estoque:
-        return produtos_vendidos[id_estoque]
+        return produtos_estoque[id_estoque]
     else:
         return {"Erro: ID produto inexistente"}
 
